f169ca521c13f98cd0eefe4187cba22ace8c794b
ClaireMarieV <>
2021-04-11 16:41:02 +0200

    initialization backend

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c11dacff9fcda4614fa8741f11d4c540'),
+    },
+  },
+});
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '534c5a69-4679-4791-b03b-d625ba89d9aa'
+};
