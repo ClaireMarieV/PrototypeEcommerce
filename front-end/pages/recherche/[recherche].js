2086c25138d3c2a6d0bfc7e2a2b04615f41d1d0f
ClaireMarieV <>
2021-04-15 15:28:50 +0200

    added echo product depending to search

--- front-end/pages/recherche/[recherche].js
+++ front-end/pages/recherche/[recherche].js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
+import ProductList from "../../components/ProductList";
 import { useRouter } from "next/router";
 
 const Search = (product) => {
   const router = useRouter();
   const { recherche } = router.query;
-  const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -17,7 +17,7 @@ const Search = (product) => {
     if (recherche) {
       setLoading(true);
       setError(null);
-      fetch("/api/produits/search/" + search)
+      fetch("/api/produits/search/" + recherche)
         .then((response) => response.json())
         .then((result) => {
           setProducts(result);
@@ -36,14 +36,13 @@ const Search = (product) => {
         <span>
           Votre recherche : <span>{recherche}</span>
         </span>
-
+        <div>
+          <ProductList products={products} />
+        </div>
         <style jsx>{`
           .search {
             display: inline-flex;
             gap: 1rem;
-          }
-          .search img {
-            width: 2rem;
           }
         `}</style>
       </div>
