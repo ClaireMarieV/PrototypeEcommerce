65e4934d6e858f1e8fc75d7e8295d084e7227934
ClaireMarieV <>
2021-04-17 22:26:24 +0200

    added component OneColumn

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
 const IndexPage = () => <Layout></Layout>;
--- front-end/pages/profil.js
+++ front-end/pages/profil.js
@@ -1,41 +1,42 @@
 import Layout from "../components/Layout";
+import OneColumn from "../components/OneColumn";
 
 const ProfilPage = () => {
   return (
     <Layout>
-      <div className="form">
+      <OneColumn>
         <h2>Bienvenue sur votre profil</h2>
-
-        <div className="div">
-          <input placeholder="Nom" />
+        <div className="form">
+          <div className="div">
+            <input placeholder="Nom" />
+          </div>
+          <div className="div">
+            <input placeholder="Prénom" />
+          </div>
+          <div className="div">
+            <input placeholder="Adresse" />
+          </div>
+          <div className="div">
+            <input placeholder="Ville" />
+          </div>
+          <div className="div">
+            <input placeholder="Code postal" />
+          </div>
+          <div className="div">
+            <input placeholder="Email" />
+          </div>
+          <div className="div">
+            <input placeholder="Numéro de téléphone" />
+          </div>
         </div>
-        <div className="div">
-          <input placeholder="Prénom" />
-        </div>
-        <div className="div">
-          <input placeholder="Adresse" />
-        </div>
-        <div className="div">
-          <input placeholder="Ville" />
-        </div>
-        <div className="div">
-          <input placeholder="Code postal" />
-        </div>
-        <div className="div">
-          <input placeholder="Email" />
-        </div>
-        <div className="div">
-          <input placeholder="Numéro de téléphone" />
-        </div>
-      </div>
+      </OneColumn>
       <style jsx>{`
         .form {
           display: grid;
           grid-template-columns: 1fr 1fr;
-          grid-gap: 1rem;
-          align-self: center;
-          justify-self: center;
-          justify-items: center;
+          grid-gap: 2rem;
+          width: 60vw;
+          margin: auto;
         }
         .div {
           flex-direction: column;
