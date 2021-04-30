932e1495f4213950b28b021aae2448fe3592cda3
ClaireMarieV <>
2021-04-30 10:40:10 +0200

    changed column 1fr to auto

--- front-end/components/TwoColumns.js
+++ front-end/components/TwoColumns.js
@@ -4,7 +4,7 @@ const TwoColumns = ({ children }) => (
     <style jsx>{`
       .container {
         display: grid;
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(2, auto);
         justify-self: center;
         grid-gap: 2rem;
         height: 100%;
