d16289b7904cab593a3f6a1abab4ab8439aa2be7
ClaireMarieV <>
2021-05-04 15:08:38 +0200

    added filter

--- front-end/pages/categorie/[slug].js
+++ front-end/pages/categorie/[slug].js
@@ -3,13 +3,35 @@ import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import ProductList from "../../components/ProductList";
 import Filter from "../../components/Filter";
+import TwoColumns from "../../components/TwoColumns";
+import OneColumn from "../../components/OneColumn";
+
 const Category = () => {
   const router = useRouter();
   const { slug } = router.query;
   const [category, setCategory] = useState(null);
+  const [produits, setProduits] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const loadProducts = () => {
+    setLoading(true);
+    setError(null);
+    fetch("/api/produits?skip=" + produits.length)
+      .then((response) => response.json())
+      .then((p) => {
+        setProduits(produits.concat(p));
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
   useEffect(() => {
     if (slug) {
       fetch("/api/categories/" + slug)
@@ -26,7 +48,28 @@ const Category = () => {
   }, [slug]);
 
   return (
-    <Layout>{category && <ProductList products={category.produits} />}</Layout>
+    <Layout>
+      {category && (
+        <>
+          <TwoColumns>
+            <Filter />
+            <OneColumn>
+              <ProductList products={category.produits} />
+              <button onClick={loadProducts}>Load More</button>
+            </OneColumn>
+          </TwoColumns>
+        </>
+      )}
+      <style jsx>{`
+        button {
+          width: fit-content;
+          height: fit-content;
+          padding: 1rem;
+          border-radius: 5px;
+          justify-self: center;
+        }
+      `}</style>
+    </Layout>
   );
 };
 
