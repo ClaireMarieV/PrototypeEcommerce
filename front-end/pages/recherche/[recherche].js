6cb75d0de363bd0bb90d96ab9361a14f4fa428c0
ClaireMarieV <>
2021-05-06 11:34:18 +0200

    added input search bar

--- front-end/pages/recherche/[recherche].js
+++ front-end/pages/recherche/[recherche].js
@@ -4,13 +4,17 @@ import ProductList from "../../components/ProductList";
 import { useRouter } from "next/router";
 import Loading from "../../components/Loading";
 import Error from "../../components/Error";
+import OneColumn from "../../components/OneColumn";
 
 const Search = (product) => {
   const router = useRouter();
   const { recherche } = router.query;
+  const [search, setSearch] = useState({ recherche });
+
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
   const value = (event) => {
     setSearch(event.target.value);
   };
@@ -37,20 +41,32 @@ const Search = (product) => {
       {error && <Error />}
       {loading && <Loading />}
       {!loading && !error && (
-        <div className="search">
-          <span>
-            Votre recherche : <span>{recherche}</span>
-          </span>
-          <div>
+        <OneColumn>
+          <div className="search">
+            <input
+              type="text"
+              value={search}
+              onKeyUp={(event) => {
+                console.log(event);
+                if (event.keyCode === 13) {
+                  router.push("/recherche/" + search);
+                }
+              }}
+              onChange={value}
+            />
             <ProductList products={products} />
+            <style jsx>{`
+              .search {
+                display: flex;
+                gap: 3rem;
+                flex-direction: column;
+              }
+              input {
+                font-size: 2rem;
+              }
+            `}</style>
           </div>
-          <style jsx>{`
-            .search {
-              display: inline-flex;
-              gap: 1rem;
-            }
-          `}</style>
-        </div>
+        </OneColumn>
       )}
     </Layout>
   );
