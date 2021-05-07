941cf23ae46b6bec9bcde594e24daa6034cc5b1d
ClaireMarieV <>
2021-05-07 18:30:21 +0200

    added label h1 and place price

--- front-end/components/Product.js
+++ front-end/components/Product.js
@@ -3,63 +3,68 @@ import ShopButton from "../components/ShopButton";
 import Flex from "../components/Flex";
 import Image from "next/image";
 
-const Product = ({ product }) => (
-  <Flex>
-    <div className="images">
-      {product.images.map((image) => (
-        <Image
-          src={process.env.NEXT_PUBLIC_BACKOFFICE_HOST + image.url}
-          width={500}
-          height={700}
-        />
-      ))}
-    </div>
-    <div className="info">
-      <div className="label">
-        <span>{product.label}</span>
+const Product = ({ product }) => {
+  return (
+    <Flex>
+      <div className="images">
+        {product.images.map((image) => (
+          <Image
+            src={process.env.NEXT_PUBLIC_BACKOFFICE_HOST + image.url}
+            width={500}
+            height={700}
+          />
+        ))}
       </div>
-      <div>
+      <div className="info">
+        <div className="label">
+          <h1>{product.label}</h1>
+          <div className="price">
+            <span>__{product.prix}€</span>
+          </div>
+        </div>
         <span>REF.{product.reference}</span>
-      </div>
-      <div>
-        <span>{product.description}</span>
-      </div>
-      <div className="price">
-        <span>{product.prix}€</span>
-      </div>
-      <ShopButton product={product} />
-    </div>
-    <style jsx>{`
-      .one-product {
-        display: flex;
-        gap: 2rem;
-        justify-items: center;
-        position: relative;
-        margin: auto;
-      }
-      .images {
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-        gap: 2rem;
-      }
 
-      .info {
-        position: -webkit-sticky;
-        position: sticky;
-        top: 10px;
-        gap: 1rem;
-        display: flex;
-        flex-direction: column;
-      }
-      .label > span,
-      .price > span {
-        text-transform: uppercase;
-        font-weight: 600;
-      }
-    `}</style>
-  </Flex>
-);
+        <span>{product.description}</span>
+
+        <ShopButton product={product} />
+        <style jsx>{`
+          .images {
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            gap: 2rem;
+          }
+          .lable {
+            display: flex;
+            flex-direction: column;
+          }
+          .info {
+            display: flex;
+            flex-direction: column;
+            align-self: flex-start;
+            position: -webkit-sticky;
+            position: sticky;
+            top: 0;
+            gap: 2rem;
+            width: 100%;
+          }
+          .price > span {
+            text-transform: uppercase;
+            font-weight: 600;
+            font-size: 1.2rem;
+            color: #373737;
+            letter-spacing: 0;
+          }
+          h1 {
+            font-size: 1.5rem;
+            text-transform: uppercase;
+            letter-spacing: inherit;
+          }
+        `}</style>
+      </div>
+    </Flex>
+  );
+};
 
 export default Product;
