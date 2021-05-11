7ceecfac515c4d7cf291f7826c3b5ae68b47010f
ClaireMarieV <>
2021-05-11 17:13:54 +0200

    added import image

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -7,11 +7,19 @@ import {
   Text9,
   Text12,
 } from "../components/gridIndex/Text";
+import {
+  Image3,
+  Image4,
+  Image6,
+  Image9,
+  Image12,
+} from "../components/gridIndex/Image";
 
 const IndexPage = () => (
   <Layout>
     <HorizontalFocus>
       <Text12 text="New collection" />
+      <Image9 src="/svg/bauhaus.svg" />
     </HorizontalFocus>
     <style jsx>{``}</style>
   </Layout>
