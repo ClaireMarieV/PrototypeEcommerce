e338f3f245fb0d20534161405909ff5920498510
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-15 22:58:57 +0200

    changed three columns to four columns in page

--- front-end/components/ProductList.js
+++ front-end/components/ProductList.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ThreeColumns from "../components/ThreeColumns";
+import FourColumns from "../components/FourColumns";
 import ShopButton from "../components/ShopButton";
 import Image from "next/image";
 import sub from "date-fns/sub";
@@ -9,7 +9,7 @@ const ProductList = ({ products }) => {
     months: 1,
   });
   return (
-    <ThreeColumns>
+    <FourColumns>
       {products.map((product) => (
         <Link key={product.id} href={"/produit/" + product.slug}>
           <a>
@@ -59,7 +59,7 @@ const ProductList = ({ products }) => {
           text-transform: uppercase;
         }
       `}</style>
-    </ThreeColumns>
+    </FourColumns>
   );
 };
 export default ProductList;
