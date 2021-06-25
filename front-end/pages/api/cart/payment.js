659b2a19ca7dc9f862643c90f6440c98ba43bf5d
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-25 16:45:26 +0200

    removed test code

--- front-end/pages/api/cart/payment.js
+++ front-end/pages/api/cart/payment.js
@@ -1,39 +1,6 @@
 import stripe from "../../../lib/stripe";
-const products = [
-  {
-    id: 1,
-    label: "bouquet",
-    price: 50,
-    images: [
-      {
-        formats: {
-          medium: {
-            url:
-              "https://grandajacciobaleone.com/wp-content/uploads/2019/02/logo-zara.jpg",
-          },
-        },
-      },
-    ],
-  },
-  {
-    id: 1,
-    label: "bouquet",
-    price: 50,
-    images: [
-      {
-        formats: {
-          medium: {
-            url:
-              "https://grandajacciobaleone.com/wp-content/uploads/2019/02/logo-zara.jpg",
-          },
-        },
-      },
-    ],
-  },
-];
 
 export default async (req, res) => {
-  console.log(req.body);
   const session = await stripe.createSession(req.headers.host, req.body);
 
   res.status(200).json(session);
