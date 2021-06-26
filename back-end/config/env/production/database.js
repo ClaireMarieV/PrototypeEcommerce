eb4dca2267d9f664b8fcf06c3615cae8afbe047a
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-26 12:28:53 +0200

    added username to config database

--- back-end/config/env/production/database.js
+++ back-end/config/env/production/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
         host: config.host,
         port: config.port,
         database: config.database,
-        username: config.user,
+        username: config.username,
         password: config.password,
         ssl: {
           rejectUnauthorized: false,
