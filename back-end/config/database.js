3e5ac7bf361f44541cb09a978a8be0b79a8becfb
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-30 15:53:07 +0200

    Added option ssl

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -19,9 +19,12 @@ module.exports = ({ env }) => {
           database: config.database,
           username: config.user,
           password: config.password,
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+          },
         },
         options: {
-          ssl: false,
+          ssl: true,
         },
       },
     },
