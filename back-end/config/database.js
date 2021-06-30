393d7d399fb6e50d8cd22b44a4bec28637d5d5a5
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-30 15:43:08 +0200

    changed to name emv devpostgres

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,25 +1,29 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(env("DATABASE_URL", "localhost"));
 
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+  const config = parse(
+    env(
+      "DATABASE_URL",
+      "postgres://postgres:S*ezane88@localhost:5432/ecommerceHabaah"
+    )
+  );
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+        },
+        options: {
+          ssl: false,
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  };
+};
