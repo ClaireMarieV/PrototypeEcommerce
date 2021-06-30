ff7464fcbaa5864cfa52e9ed182012cbd6ddafc8
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-06-30 16:26:01 +0200

    test - deleted var env database SSL

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => {
           username: config.user,
           password: config.password,
           ssl: {
-            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+            rejectUnauthorized: env.bool(false), // For self-signed certificates
           },
         },
         options: {
