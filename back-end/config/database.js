b1a1f1ae4444dc092fa1e231e4a49ae81748fc74
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-07-01 11:57:49 +0200

    added ssl

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -19,6 +19,9 @@ module.exports = ({ env }) => {
           database: config.database,
           username: config.user,
           password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
         options: { ssl: true },
       },
