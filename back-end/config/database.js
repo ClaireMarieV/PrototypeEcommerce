7ce18b5308f69d9fae057d4bf4e29105796ffa66
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-07-05 22:49:43 +0200

    deleted ssl true in option

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -19,11 +19,8 @@ module.exports = ({ env }) => {
           database: config.database,
           username: config.user,
           password: config.password,
-          ssl: {
-            rejectUnauthorized: false,
-          },
         },
-        options: { ssl: true },
+        options: {},
       },
     },
   };
