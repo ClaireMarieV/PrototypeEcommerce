1aa1bd073c71d85936c36edbf073c058a95724e7
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-07-06 22:31:24 +0200

    added cloudinary url

--- back-end/config/env/production/plugins.js
+++ back-end/config/env/production/plugins.js
@@ -6,6 +6,9 @@ module.exports = ({ env }) => ({
       cloud_name: env("claire-marie-vaney"),
       api_key: env("442833242335429"),
       api_secret: env("rDBIrMlIy_sGLPSHSVXhbbyts94"),
+      cloudinary_url: env(
+        "cloudinary://442833242335429:rDBIrMlIy_sGLPSHSVXhbbyts94@claire-marie-vaney"
+      ),
     },
   },
   // ...
