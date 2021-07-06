ce6ba10f7ee040acb4c9a7b58457e7c72220c425
ClaireMarieV <claire.marie.vaney@gmail.com>
2021-07-06 10:25:48 +0200

    renamed files server and deleted default value jwt secret

--- back-end/config/env/production.js/server.js
+++ back-end/config/env/production.js/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET", "c11dacff9fcda4614fa8741f11d4c540"),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
